fdbe4814ee1ffca332561d1421380014efe01297
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-04-10 09:41:43 -0600

    Started completing the methods in the shape canvas class.

--- src/art/view/ShapeCanvas.java
+++ src/art/view/ShapeCanvas.java
@@ -32,12 +32,42 @@ public class ShapeCanvas extends JPanel
 	
 	public ShapeCanvas(ArtController app)
 	{
+		super();
+		this.app = app;
+		triangleList = new ArrayList<Polygon>();
+		polygonList = new ArrayList<Polygon>();
+		ellipseList = new ArrayList<Ellipse2D>();
+		rectangleList = new ArrayList<Rectangle>();
 		
+		canvasImage = new BufferedImage(600, 600, BufferedImage.TYPE_INT_ARGB);
+		this.setMinimumSize(new Dimension(600,600));
+		this.setPreferredSize(new Dimension(600,600));
+		this.setMaximumSize(getPreferredSize());
 	}
 	
 	public void addShape(Shape current)
 	{
+		if (current instanceof Polygon)
+		{
+			if (((Polygon)current).xpoints.length == 3)
+			{
+				triangleList.add((Polygon) current);
+			}
+			else
+			{
+				polygonList.add((Polygon) current);
+			}
+		}
+		else if (current instanceof Ellipse2D)
+		{
+			ellipseList.add((Ellipse2D) current);
+		}
+		else
+		{
+			rectangleList.add((Rectangle) current);
+		}
 		
+		updateImage();
 	}
 	
 	public void clear()
@@ -65,8 +95,10 @@ public class ShapeCanvas extends JPanel
 		
 	}
 	
+	@Override
 	protected void paintComponent(Graphics graphics)
 	{
-		
+		super.paintComponent(graphics);
+		graphics.drawImage(canvasImage, 0, 0, null);
 	}
 }
