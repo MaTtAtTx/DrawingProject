32dc9f2e65e0798487b650303055dc391a216f05
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-04-19 07:55:58 -0600

    Finished the shape canvas code, but there is an error.

--- src/art/view/ShapeCanvas.java
+++ src/art/view/ShapeCanvas.java
@@ -90,12 +90,27 @@ public class ShapeCanvas extends JPanel
 	
 	public void save()
 	{
-		
+		try
+		{
+			JFileChooser saveDialog = new JFileChooser();
+			saveDialog.showSaveDialog(app.getFrame());
+			String savePath = saveDialog.getSelectedFile().getPath();
+			ImageIO.write(canvasImage, "PNG", new File(savePath));
+		}
+		catch (IOException error)
+		{
+			app.handleErrors(error);
+		}	
 	}
 	
 	private Color randomColor()
 	{
-		return null;
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		int alpha = (int) (Math.random() * 256);
+		
+		return new Color(red, green, blue, alpha);
 	}
 	
 	private void updateImage()
